3ccd60fc7392377d6d92231f9bd7bbd6baf42b1b
vavarine <evailson.m.barbosa@gmail.com>
2022-10-04 21:34:51 -0300

    Add Note & NotesList

--- src/components/AddNote/index.tsx
+++ src/components/AddNote/index.tsx
@@ -1,9 +1,62 @@
-export function AddNote() {
+import { useEffect, useState } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+import axios from "axios";
+import { api } from "../../services/api";
+
+export type Note = {
+  title: string;
+  text: string;
+};
+
+interface AddNoteProps {
+  onAddNote: () => void;
+}
+
+export function AddNote({ onAddNote }: AddNoteProps) {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const note = {
+      title: title,
+      text: text,
+    };
+
+    api
+      .post("/notes", note)
+      .then(() => {
+        onAddNote();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <FiCheckSquare />
       </div>
-    </div>
+      <div>
+        <textarea
+          name="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></textarea>
+      </div>
+      <button type="submit">Salvar</button>
+    </form>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import { AddNote } from "../../components/AddNote";
+import { useEffect, useState } from "react";
+import { AddNote, Note } from "../../components/AddNote";
+import { NotesList } from "../../components/NotesList";
+import { api } from "../../services/api";
 
 function Home() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const handleAddNote = () => {
+    fechNotes();
+  };
+
+  const fechNotes = () => {
+    api.get("/notes").then((response) => {
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fechNotes();
+  }, []);
+
   return (
     <div>
-      <AddNote />
+      <AddNote onAddNote={handleAddNote} />
+      <NotesList notes={notes} />
     </div>
   );
 }
